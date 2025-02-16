1b568195d629726e4baddb4b2882b532489e06d1
Victor Komlev <victor.v.komlev@gmail.com>
2025-02-16 13:59:58 +0500

    Документация запускаемого модуля

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,15 @@
-# wsgi.py
+# ./wsgi.py
+
+"""Модуль запуска WSGI-сервера для приложения SKU_sinergy.
+
+Предназначен для определения точки входа WSGI-сервера.
+"""
 
 from app.main import create_app
 
-app = create_app()
+# Создание экземпляра приложения
+application = create_app()
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    
+if __name__ == "__main__":
+    # Запуск приложения для отладки
+    application.run(host="0.0.0.0", port=5000, debug=True)
